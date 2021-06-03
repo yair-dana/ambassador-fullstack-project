0d433504dac53086c2d759728224a1dc21f0dcce
Yair Dana <yaird@wix.com>
2021-06-03 19:53:35 +0300

    refactor render comments list

--- src/components/CommentsList/CommentsList.tsx
+++ src/components/CommentsList/CommentsList.tsx
@@ -20,33 +20,44 @@ type CommentsListProps = {
 };
 
 export default function CommentsList(props: CommentsListProps) {
+  const showNoResultMessage: boolean =
+    props.comments === undefined && props.status.skin === 'success';
+
+  const showComments: boolean = props.comments !== undefined;
+
+  const renderCommentsList = () => {
+    return props.comments?.map((comment, index) => {
+      return (
+        <Cell key={`cell-${index}`}>
+          <Text
+            key={`text-${index}`}
+            dataHook={`${DataHooks.COMMENT}-${index}`}
+          >
+            {CommentToString(comment.author, comment.text)}
+          </Text>
+        </Cell>
+      );
+    });
+  };
+
+  const renderNoResultMessage = () => {
+    return (
+      <Cell>
+        <Heading dataHook={DataHooks.RESULT_MESSAGE} appearance="H5">
+          Your site has no comments
+        </Heading>
+      </Cell>
+    );
+  };
+
   return (
     <Card>
       <Card.Header title="Comments List" />
       <Card.Divider />
       <Card.Content>
         <Layout>
-          {props.comments != undefined &&
-            props.comments?.map((comment, index) => {
-              return (
-                <Cell key={`cell-${index}`}>
-                  <Text
-                    key={`text-${index}`}
-                    dataHook={`${DataHooks.COMMENT}-${index}`}
-                  >
-                    {CommentToString(comment.author, comment.text)}
-                  </Text>
-                </Cell>
-              );
-            })}
-
-          {props.comments === undefined && props.status.skin === 'success' && (
-            <Cell>
-              <Heading dataHook={DataHooks.RESULT_MESSAGE} appearance="H5">
-                Your site has no comments
-              </Heading>
-            </Cell>
-          )}
+          {showComments && renderCommentsList()}
+          {showNoResultMessage && renderNoResultMessage()}
         </Layout>
       </Card.Content>
     </Card>

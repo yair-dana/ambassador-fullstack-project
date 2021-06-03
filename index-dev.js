d4472a1553eb707f36fb504ac5063cb25808a350
Yair Dana <yaird@wix.com>
2021-06-03 13:09:39 +0300

    Add DataHooks and Mock for locally test

--- index-dev.js
+++ index-dev.js
@@ -1,8 +1,28 @@
 const { bootstrapServer, emitConfigs } = require('./environment');
 
+import { AmbassadorTestkit } from '@wix/ambassador-testkit';
+import { NodeWorkshopScalaApp } from '@wix/ambassador-node-workshop-scala-app/rpc';
+
+const dummyCommentList = [
+  { author: 'author1', text: 'dummy comment' },
+  { author: 'author2', text: 'dummy comment' },
+  { author: 'author3', text: 'dummy comment' },
+];
+
 (async () => {
   const app = bootstrapServer();
+  /** Start RPC Mocks **/
+  const ambassadorTestkit = new AmbassadorTestkit();
+  const siteId = '1234';
 
+  ambassadorTestkit
+    .createStub(NodeWorkshopScalaApp)
+    .CommentsService()
+    .fetch.when(siteId)
+    .resolve(dummyCommentList);
+
+  await ambassadorTestkit.start();
+  /** End RPC Mocks **/
   await emitConfigs();
   await app.start();
 })();
---
+++ src/DataHooks.ts
@@ -0,0 +1,6 @@
+export default {
+  PAGE_HEADER: 'page-header',
+  ADD_COMMENT: 'add-button',
+  FETCH_COMMENTS: 'fetch-button',
+  COMMENT: 'comment-data',
+};

8bcc5a28189eecb2e399146e4cbaf4d11147dc3e
Yair Dana <yaird@wix.com>
2021-06-03 13:08:25 +0300

    Create App Layout and General Tests

--- src/components/App/App.spec.tsx
+++ src/components/App/App.spec.tsx
@@ -1,21 +1,64 @@
 import React, { Suspense } from 'react';
 import { render } from '@testing-library/react';
-import { I18nextProvider } from '@wix/wix-i18n-config';
-import i18n from '../__mocks__/i18n';
 import App from './App';
+import {
+  PageHeaderTestkit,
+  ButtonTestkit,
+  TextTestkit,
+} from 'wix-style-react/dist/testkit';
+import DataHooks from '../../DataHooks';
+import { act } from 'react-dom/test-utils';
+
+import { baseUrl } from '@wix/ambassador-testkit/dist/test/fixtures/html-app/config';
+import { CommentToString } from '../../utils';
+
+const dummyCommentList = [
+  { author: 'author1', text: 'dummy comment' },
+  { author: 'author2', text: 'dummy comment' },
+  { author: 'author3', text: 'dummy comment' },
+];
+
+import axios from 'axios';
+
+import MockAdapter from 'axios-mock-adapter';
+
+const axiosMock = new MockAdapter(axios);
 
 describe('App', () => {
   it('renders a title correctly', async () => {
-    const { findByTestId } = render(
-      <Suspense fallback="...loading">
-        <I18nextProvider i18n={i18n}>
-          <App />
-        </I18nextProvider>
-      </Suspense>,
+    const { baseElement } = render(<App />);
+
+    const title = await PageHeaderTestkit({
+      wrapper: baseElement,
+      dataHook: DataHooks.PAGE_HEADER,
+    });
+
+    expect(await title.titleText()).toEqual('Comments App');
+  });
+
+  it('should render comments when user click fetch', async () => {
+    const { baseElement } = render(<App />);
+
+    const commentsUri = '/comments';
+    const url = new RegExp(`${commentsUri}*`);
+    axiosMock.onGet(url).reply(200, dummyCommentList);
+
+    const fetchButton = await ButtonTestkit({
+      wrapper: baseElement,
+      dataHook: DataHooks.FETCH_COMMENTS,
+    });
+
+    await act(async () => {
+      await fetchButton.click();
+    });
+
+    const firstComment = await TextTestkit({
+      wrapper: baseElement,
+      dataHook: `${DataHooks.COMMENT}-0`,
+    });
+
+    expect(await firstComment.getText()).toEqual(
+      CommentToString(dummyCommentList[0].author, dummyCommentList[0].text),
     );
-
-    const el = await findByTestId('app-title');
-
-    expect(el.textContent).toBe('app.title');
   });
 });
--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,21 +1,119 @@
-import React from 'react';
-import { withTranslation, WithTranslation } from '@wix/wix-i18n-config';
-import s from './App.scss';
+import React, { useState } from 'react';
+import {
+  Page,
+  WixStyleReactProvider,
+  Breadcrumbs,
+  Text,
+  Card,
+  Box,
+  Cell,
+  Input,
+  Layout,
+  FontUpgrade,
+  FormField,
+} from 'wix-style-react';
+import DataHooks from '../../DataHooks';
+import ActiveBar from '../ActiveBar/ActiveBar';
+import axios from 'axios';
+import { BreadcrumbsItem } from 'wix-style-react/dist/es/src/Breadcrumbs';
+import { Comment } from '@wix/ambassador-node-workshop-scala-app/rpc';
+import * as events from 'events';
+import { CommentToString } from '../../utils';
 
-interface AppProps extends WithTranslation {}
+function App() {
+  const [commentsList, setCommentList] = useState<undefined | Comment[]>(
+    undefined,
+  );
+  const [siteId, setSiteId] = useState('1234');
 
-class App extends React.Component<AppProps> {
-  render() {
-    const { t } = this.props;
+  const addComment = async (e: any) => {
+    e.preventDefault();
+  };
 
-    return (
-      <div className={s.root}>
-        <h2 className={s.title} data-hook="app-title">
-          {t('app.title', { who: 'Yoshi' })}
-        </h2>
-      </div>
-    );
-  }
+  const fetchComments = async (e: any) => {
+    e.preventDefault();
+    const comments = await axios.get(`/comments?siteId=1234`);
+
+    if (comments.status === 200) {
+      setCommentList(comments.data);
+    }
+  };
+
+  const breadcrumbItems: BreadcrumbsItem[] = [
+    { id: 1, value: 'Root Page' },
+    { id: 2, value: 'Comments App' },
+  ];
+
+  return (
+    <WixStyleReactProvider features={{ reducedSpacingAndImprovedLayout: true }}>
+      <FontUpgrade>
+        <Page height="100vh">
+          <Page.Header
+            dataHook={DataHooks.PAGE_HEADER}
+            title="Comments App"
+            breadcrumbs={<Breadcrumbs items={breadcrumbItems} activeId={2} />}
+            actionsBar={
+              <ActiveBar
+                onAddComment={addComment}
+                onFetchComments={fetchComments}
+              />
+            }
+          />
+          <Page.Content>
+            <Layout>
+              <Cell span={6}>
+                <Card>
+                  <Card.Header title="Add Comment" />
+                  <Card.Divider />
+                  <Card.Content>
+                    <Layout>
+                      <Cell span={6}>
+                        <FormField label="Author" required>
+                          <Input />
+                        </FormField>
+                      </Cell>
+                      <Cell span={6}>
+                        <FormField label="Text" required>
+                          <Input />
+                        </FormField>
+                      </Cell>
+                    </Layout>
+                  </Card.Content>
+                </Card>
+              </Cell>
+              <Cell span={6}>
+                <Card>
+                  <Card.Header title="Comments List" />
+                  <Card.Divider />
+                  <Card.Content>
+                    <Layout>
+                      {commentsList != undefined
+                        ? commentsList?.map((comment, index) => {
+                            return (
+                              <Cell key={`cell-${index}`}>
+                                <Text
+                                  key={`text-${index}`}
+                                  dataHook={`${DataHooks.COMMENT}-${index}`}
+                                >
+                                  {CommentToString(
+                                    comment.author,
+                                    comment.text,
+                                  )}
+                                </Text>
+                              </Cell>
+                            );
+                          })
+                        : null}
+                    </Layout>
+                  </Card.Content>
+                </Card>
+              </Cell>
+            </Layout>
+          </Page.Content>
+        </Page>
+      </FontUpgrade>
+    </WixStyleReactProvider>
+  );
 }
 
-export default withTranslation()(App);
+export default App;

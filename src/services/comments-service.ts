5e4a27dd7135ada621f3508ad885e6570ed84a0c
Yair Dana <yaird@wix.com>
2021-06-06 10:34:52 +0300

    Remove unused function

--- src/services/comments-service.ts
+++ src/services/comments-service.ts
@@ -1,25 +1,19 @@
-import { Comment, NodeWorkshopScalaApp } from '@wix/ambassador-node-workshop-scala-app/rpc';
+import {
+  Comment,
+  NodeWorkshopScalaApp,
+} from '@wix/ambassador-node-workshop-scala-app/rpc';
 
-export const fetchComments = async(aspect:any, id:string) => {
+export const fetchComments = async (aspect: any, id: string) => {
   const commentsService = NodeWorkshopScalaApp().CommentsService();
-  const comments: Promise<Comment[]> = commentsService(aspect).fetch(
-    id
-  );
+  const comments: Promise<Comment[]> = commentsService(aspect).fetch(id);
   return comments;
 };
 
-
-export const addComments = async(aspect:any, id:string, comment:Comment) => {
+export const addComments = async (
+  aspect: any,
+  id: string,
+  comment: Comment,
+) => {
   const commentsService = NodeWorkshopScalaApp().CommentsService();
-  return commentsService(aspect).add(
-    id, comment
-  );
+  return commentsService(aspect).add(id, comment);
 };
-
-//
-// export const add = method(async function (comment) {
-//   const commentsService = NodeWorkshopScalaApp().CommentsService();
-//   return commentsService(this.context.aspects).add(
-//     'a1ea1e40-8698-42e7-a75a-85cd95ffaa23', comment,
-//   );
-// });
